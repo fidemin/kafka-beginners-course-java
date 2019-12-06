0a97ac4c4f3c68cec75af8921fba20b9fad32a6d
Yunhong Min <yhmin84@gmail.com>
2019-12-06 19:38:06 +0900

    consumer demo

---
+++ src/main/java/com/github/yhmin84/kafka/tutorial1/ConsumerDemo.java
@@ -0,0 +1,51 @@
+package com.github.yhmin84.kafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "my-fourth-application";
+        String topic = "first_topic";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // lastest, none
+
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+
+        // subscribe consumer to our topic
+        consumer.subscribe(Collections.singletonList(topic));
+
+        // poll for new data
+        // read all the values from partition 0 first, partition 1 second, partition 2 third
+        // if multi messages in brokers.
+        while (true) {
+            ConsumerRecords<String, String> records
+                    = consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
+
+            for (ConsumerRecord<String, String> record: records) {
+               logger.info("Key: " + record.key() + ", Value: " + record.value());
+               logger.info("Partition: " + record.partition() + ", Offset:" + record.offset());
+            }
+        }
+    }
+}
