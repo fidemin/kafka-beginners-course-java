1fd77c7353a5caf4416fbb14b13e99de22ed3832
Yunhong Min <yhmin84@gmail.com>
2019-12-06 19:04:20 +0900

    Producer with key

--- src/main/java/com/github/yhmin84/kafka/tutorial1/ProducerDemoKeys.java
+++ src/main/java/com/github/yhmin84/kafka/tutorial1/ProducerDemoKeys.java
@@ -1,16 +1,19 @@
 package com.github.yhmin84.kafka.tutorial1;
 
-import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ExecutionException;
 
-public class ProducerDemoWithCallback {
-    public static void main(String[] args) {
-        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+public class ProducerDemoKeys {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
 
         String bootstrapServers = "127.0.0.1:9092";
 
@@ -23,11 +26,16 @@ public class ProducerDemoWithCallback {
         // create the producer <String, String> : key to be string and value to be string
         try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
             for (int i=0; i<10; i++) {
-                ProducerRecord<String, String> record = new ProducerRecord<>(
-                        "first_topic", "hello world" + i);
+                String topic = "first_topic";
+                String value = "hello world " + i;
+                String key = "id_" + i;
+
+                ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+
+                // same key always goes to the same partition!
+                logger.info("Key: " + key);
 
                 // send data - asynchronous. consumer doesn't receive any message until flush method executed
-                // the data sequence in consumer does not guarantee the sequence of input 0, 1, 2, ..., 9
                 producer.send(record, (RecordMetadata recordMetadata, Exception e) -> {
                     // execute every time a record is successfully sent or an exception is thrown
                     if (e == null) {
@@ -41,7 +49,7 @@ public class ProducerDemoWithCallback {
                         logger.error("Error while producing", e);
 
                     }
-                });
+                }).get(); // block the .send() to make it synchronous. don't do this in production
             }
             // flush data : this is essential. The data in buffer actually is sent and waiting for it.
             producer.flush();
